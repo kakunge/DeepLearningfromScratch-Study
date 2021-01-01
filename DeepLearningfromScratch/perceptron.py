85efd721d3a923a09245cd94da78779e2c41198e
kakunge <kakunge3@naver.com>
2021-01-01 19:56:28 +0900

    perceptron
    2021/01/01

--- DeepLearningfromScratch/perceptron.py
+++ DeepLearningfromScratch/perceptron.py
@@ -1,10 +1,34 @@
 #가중치와 편향의 도입
+
+#AND 게이트
 import numpy as np
 
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+#NAND 게이트
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+#OR 게이트
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = 0.2
     tmp = np.sum(w*x) + b
     if tmp <= 0:
         return 0
