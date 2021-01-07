2b7b343c514f96dd0831d65336599896b0fe0a6d
kakunge <52910968+kakunge@users.noreply.github.com>
2021-01-07 20:42:43 +0900

    Add files via upload

--- DeepLearningfromScratch/신경망_학습.py
+++ DeepLearningfromScratch/신경망_학습.py
@@ -37,3 +37,14 @@ def cross_entropy_error_batch(y, t):
         
     batch_size = y.shape[0]
     return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+#원-핫 인코딩이 아닐 경우
+'''
+def cross_entropy_error_batch(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+'''
