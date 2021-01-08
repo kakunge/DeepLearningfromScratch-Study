d1377fc92c10e4c8241fb834d968d6f337956a54
kakunge <52910968+kakunge@users.noreply.github.com>
2021-01-08 21:20:59 +0900

    Add files via upload

--- DeepLearningfromScratch/수치미분.py
+++ DeepLearningfromScratch/수치미분.py
@@ -5,3 +5,11 @@ def numerical_diff(f, x):
     h = 1e-4
     return (f(x+h) - f(x-h)) / (2*h)
 
+#간단한 함수의 수치 미분
+def function_1(x):
+    return 2*x**2-3*x+4
+
+print(numerical_diff(function_1, 5))#16.999999999995907
+print(numerical_diff(function_1, 10))#36.99999999980719
+#해석적인 결과와 거의 같게 나온다
+
