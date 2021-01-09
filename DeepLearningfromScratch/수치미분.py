8eb98dcb458adadb70fb5548df7bedb545fd0afb
kakunge <52910968+kakunge@users.noreply.github.com>
2021-01-09 22:41:28 +0900

    Add files via upload

--- DeepLearningfromScratch/수치미분.py
+++ DeepLearningfromScratch/수치미분.py
@@ -14,3 +14,23 @@ print(numerical_diff(function_1, 10))#36.99999999980719
 #해석적인 결과와 거의 같게 나온다
 
 #편미분 : 해석적인 과정과 비슷하게 하나의 변수에 초점을 맞춰서 새로운 함수를 정의하여 수치 미분 함수를 적용한다.
+
+#기울기
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[isx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+    
+    return grad
+
