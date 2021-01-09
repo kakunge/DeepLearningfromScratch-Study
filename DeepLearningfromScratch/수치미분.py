ea1b70b456769a95f7dd82d14381dbc8e9bb0633
kakunge <52910968+kakunge@users.noreply.github.com>
2021-01-09 22:46:26 +0900

    Add files via upload

--- DeepLearningfromScratch/수치미분.py
+++ DeepLearningfromScratch/수치미분.py
@@ -34,3 +34,12 @@ def numerical_gradient(f, x):
     
     return grad
 
+#경사 하강법 - 최적화
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
+
