e388b0373e08d9f5214a52557b9d310dfad11edf
kakunge <52910968+kakunge@users.noreply.github.com>
2021-01-09 22:43:27 +0900

    Add files via upload

--- DeepLearningfromScratch/수치미분.py
+++ DeepLearningfromScratch/수치미분.py
@@ -29,7 +29,7 @@ def numerical_gradient(f, x):
         x[idx] = tmp_val - h
         fxh2 = f(x)
 
-        grad[isx] = (fxh1 - fxh2) / (2*h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
         x[idx] = tmp_val
     
     return grad
