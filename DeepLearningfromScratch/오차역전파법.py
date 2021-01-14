d38b0d04dfd6c0d504acfe302690c6051e20e38b
Kyohyun Lee <52910968+kakunge@users.noreply.github.com>
2021-01-14 20:44:29 +0900

    Add files via upload

--- DeepLearningfromScratch/오차역전파법.py
+++ DeepLearningfromScratch/오차역전파법.py
@@ -37,3 +37,22 @@ class addLayer:
         dy = dout * 1
 
         return dx, dy
+
+#ReLU 계층
+class ReLU:
+    def __init__(self):
+        self.mask = None
+    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
+
