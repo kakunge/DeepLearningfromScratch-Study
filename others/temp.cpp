e914cec105d17d0e9696c9c4765729599ef2c6f3
Kyohyun Lee <52910968+kakunge@users.noreply.github.com>
2021-02-03 18:56:43 +0900

    Create temp.cpp

---
+++ others/temp.cpp
@@ -0,0 +1,10 @@
+std::vector<int> factor(int n) {
+    if(n == 1) return vector<int>(1, 1);
+    vector<int> ret;
+    for(int div = 2; n > 1; ++div)
+        while(n % div == 0) {
+            n /= div;
+            ret.push_back(div);
+        }
+    return ret;
+}
