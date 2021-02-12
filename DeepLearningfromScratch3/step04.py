bc06865750dd80e235299c6fd91f7aeeba109d31
Kyohyun Lee <52910968+kakunge@users.noreply.github.com>
2021-02-12 14:03:17 +0900

    Add files via upload

--- DeepLearningfromScratch3/step04.py
+++ DeepLearningfromScratch3/step04.py
@@ -10,3 +10,24 @@ def numerical_diff(f, x, eps=1e-4):
 
     return (y1.data - y0.data) / (2 * eps)
 
+from step02 import Square
+
+f = Square()
+x = Variable(np.array(2.0))
+dy = numerical_diff(f, x)
+
+print(dy)#4.000000000004
+
+from step03 import Exp
+
+def f(x):
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    return C(B(A(x)))
+
+x = Variable(np.array(0.5))
+dy = numerical_diff(f, x)
+
+print(dy)#3.2974426293330694
