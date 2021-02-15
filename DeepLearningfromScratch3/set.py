856809ac3996482b6a8d6027eb353fe3e2f80586
Kyohyun Lee <52910968+kakunge@users.noreply.github.com>
2021-02-15 17:03:25 +0900

    Add files via upload

--- DeepLearningfromScratch3/set.py
+++ DeepLearningfromScratch3/set.py
@@ -22,12 +22,28 @@ class Function:
 
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = x ** 2
+
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+
+        return gx
 
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
 
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+
+        return gx
+        
 def numerical_diff(f, x, eps=1e-4):
     x0 = Variable(x.data - eps)
     x1 = Variable(x.data + eps)
