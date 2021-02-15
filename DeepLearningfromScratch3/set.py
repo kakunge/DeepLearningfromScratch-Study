ed08a7e6bb7f4d07877c715ee4721162fe92ff33
Kyohyun Lee <52910968+kakunge@users.noreply.github.com>
2021-02-15 16:59:21 +0900

    Add files via upload

--- DeepLearningfromScratch3/set.py
+++ DeepLearningfromScratch3/set.py
@@ -3,16 +3,21 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None#역전파에 대응하기 위한 미분값 저장
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input#입력 변수를 기억
         
         return output
 
     def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
         raise NotImplementedError()
 
 class Square(Function):
