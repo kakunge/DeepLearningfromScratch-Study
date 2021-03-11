a6dd1c634d0b45077f5f7426e61295060c66cb99
kakunge <kakunge3@naver.com>
2021-03-11 18:25:00 +0900

    gradient descent

--- DeepLearningfromScratch3/step28.py
+++ DeepLearningfromScratch3/step28.py
@@ -8,3 +8,23 @@ y = rosenbrock(x0, x1)
 y.backward()
 
 print(x0.grad, x1.grad)#-2.0 400.0
+
+x0.cleargrad()
+x1.cleargrad()
+
+lr = 0.001#learning rate
+iters = 100000#iterations
+
+for i in range(iters):
+    print(x0, x1)
+
+    y = rosenbrock(x0, x1)
+
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
+
+    #variable(0.999999999999928) variable(0.9999999999998557)
