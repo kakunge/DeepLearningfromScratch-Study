8ced75b71c521071aa9474694f80113dfdebe0bc
kakunge <kakunge3@naver.com>
2021-05-06 20:58:54 +0900

    matmul

--- DeepLearningfromScratch3/dezero/functions.py
+++ DeepLearningfromScratch3/dezero/functions.py
@@ -110,6 +110,19 @@ class SumTo(Function):
 
         return gx
 
+class MatMul(Function):
+    def forward(self, x, W):
+        y = x.dot(W)
+
+        return y
+
+    def backward(self, gy):
+        x, W = self.inputs
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+
+        return gx, gW
+
 
 
 def reshape(x, shape):
@@ -163,3 +176,6 @@ def sum_to(x, shape):
     if x.shape == shape:
         return as_variable(x)
     return SumTo(shape)(x)
+
+def matmul(x, W):
+    return MatMul()(x, W)
--- DeepLearningfromScratch3/step41.py
+++ DeepLearningfromScratch3/step41.py
@@ -1,4 +1,6 @@
 import numpy as np
+from dezero import Variable
+import dezero.functions as F
 
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
@@ -13,3 +15,13 @@ c = np.dot(a, b)
 print(c)
 #[[19 22]
 # [43 50]]
+
+x = Variable(np.random.randn(2, 3))
+W = Variable(np.random.randn(3, 4))
+y = F.matmul(x, W)
+y.backward()
+
+print(x.grad.shape)
+print(W.grad.shape)
+#(2, 3)
+#(3, 4)
