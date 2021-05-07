be94746bc07ad62fb171cb5042a1c42d7791d965
kakunge <kakunge3@naver.com>
2021-05-07 21:30:53 +0900

    neural network

--- DeepLearningfromScratch3/dezero/functions.py
+++ DeepLearningfromScratch3/dezero/functions.py
@@ -123,7 +123,7 @@ class MatMul(Function):
 
         return gx, gW
 
-class MeanSquareError(Function):
+class MeanSquaredError(Function):
     def forward(self, x0, x1):
         diff = x0 - x1
         y = (diff ** 2).sum() / len(diff)
@@ -137,6 +137,34 @@ class MeanSquareError(Function):
         gx1 = -gx0
 
         return gx0, gx1
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+class Sigmoid(Function):
+    def forward(self, x):
+        xp = cuda.get_array_module(x)
+        # y = 1 / (1 + xp.exp(-x))
+        y = xp.tanh(x * 0.5) * 0.5 + 0.5  # Better implementation
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
 
 
 
@@ -195,5 +223,27 @@ def sum_to(x, shape):
 def matmul(x, W):
     return MatMul()(x, W)
 
-def mean_square_error(x0, x1):
-    return MeanSquareError()(x0, x1)
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
+
+def linear_simple(x, W, b=None):
+    t = matmul(x, W)
+    if b is None:
+        return t
+
+    y = t + b
+    t.data = None
+
+    return y
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+
+    return y
+
+def sigmoid(x):
+    return Sigmoid()(x)
